2ed4284970ad65c9dba776971ed51f4c9dc5ef5e
Xiaoxiong Xing <xingxiaoxiong@gmail.com>
2018-06-24 09:36:56 +0900

    new frontend design

--- README.md
+++
@@ -1,1 +0,0 @@
-# Nebulas Monopoly
--- js/utils.js
+++
@@ -1,5 +0,0 @@
-var lonlat2coord = function(lon, lat) {
-    var x = (lon + 180.0) / 360.0 * width;
-    var y = height / 180.0 * (90.0 - lat);
-    return [x, y]
-}
